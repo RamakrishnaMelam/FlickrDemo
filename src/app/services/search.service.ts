268b4384f0211ceadabd41ddcc3b43c81f197df6
Ramakrishna Melam <ram.melam@gmail.com>
2017-10-17 13:17:49 +1100

    Alignment Changes.
    Alignment Changes.

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Request, RequestMethod, Headers } from '@angular/http';
-import { Photo, Photos } from "../app.model";
+import { Photo, Photos } from '../app.model';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
@@ -12,26 +12,28 @@ export class SearchService {
   // Fetch all existing photos based on filter
     /* istanbul ignore next */
     searchPhotos(request: string, key: string): Observable<Photos[]> {
-      
-              //.. flikr URL with filter.
+
+              // .. flikr URL with filter.
+              // tslint:disable-next-line:max-line-length
               const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&&api_key=' + key + '&text=' + request + '&format=json&nojsoncallback=1';
-      
+
               // ...using get request
               return this.http.get(url)
                   // ...and calling .json() on the response to return data
                   .map((res: Response) => {
                       const result = res.json();
-                      if(result.stat !== 'ok'){
+                      if (result.stat !== 'ok') {
                           // throw errow
                           Observable.throw(result.stat || 'Server error');
                       }
-                      //return photos collection.
-                      return <Photos[]>result.photos
+
+                      // return photos collection.
+                      return <Photos[]>result.photos;
                   })
-                  //...errors if any
+                  // ...errors if any
                   .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
           }
-      
+
           /* istanbul ignore next */
           handleError(error: Response) {
               return Observable.throw({
